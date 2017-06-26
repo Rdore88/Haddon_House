8a84aae7de591608244782e72d1decf494bde83d
Robby Dore <rdore88@gmail.com>
2017-06-26 10:53:49 -0400

    Added functionality to have new circles/text to appear at certain width.

--- js/home.js
+++ js/home.js
@@ -1,12 +1,51 @@
 var circles = document.getElementById('circles');
 var widthQuery = window.matchMedia('(min-width: 900px)');
 
+function superbCircle(){
+  var outerDiv = document.createElement('div');
+  var div = document.createElement('div');
+  div.innerHTML = '<p>A</p>';
+  outerDiv.className = 'circle-container';
+  div.className = 'circle';
+  var p = document.createElement('p');
+  p.textContent = "Superb Value";
+  outerDiv.appendChild(div);
+  outerDiv.appendChild(p);
+  circles.appendChild(outerDiv);
+}
+
+function locationCircle(){
+  var outerDiv = document.createElement('div');
+  var div = document.createElement('div');
+  div.innerHTML = '<p>B</p>';
+  outerDiv.className = 'circle-container';
+  div.className = 'circle';
+  var p = document.createElement('p');
+  p.textContent = "Best In-town Location";
+  outerDiv.appendChild(div);
+  outerDiv.appendChild(p);
+  circles.appendChild(outerDiv);
+}
+
+function gorgeusCircle(){
+  var outerDiv = document.createElement('div');
+  var div = document.createElement('div');
+  div.innerHTML = '<p>C</p>';
+  outerDiv.className = 'circle-container';
+  div.className = 'circle';
+  var p = document.createElement('p');
+  p.textContent = "Gorgeus Interiors";
+  outerDiv.appendChild(div);
+  outerDiv.appendChild(p);
+  circles.appendChild(outerDiv);
+}
+
 function addCircles(mediaQuery){
   console.log('Hello World');
   if (widthQuery.matches) {
-    var p = document.createElement('p');
-    p.textContent = "Hello World";
-    circles.appendChild(p);
+    superbCircle();
+    locationCircle();
+    gorgeusCircle()
   } else {
     circles.innerHTML = '';
   }
