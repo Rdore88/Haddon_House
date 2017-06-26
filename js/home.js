aa3c098ffa31f8ac968e422c86b71fa7ad1732db
Robby Dore <rdore88@gmail.com>
2017-06-26 10:05:24 -0400

    Added media query listener, temp images and basic media query structure.

--- js/home.js
+++ js/home.js
@@ -1,1 +1,16 @@
+var circles = document.getElementById('circles');
+var widthQuery = window.matchMedia('(min-width: 900px)');
 
+function addCircles(mediaQuery){
+  console.log('Hello World');
+  if (widthQuery.matches) {
+    var p = document.createElement('p');
+    p.textContent = "Hello World";
+    circles.appendChild(p);
+  } else {
+    circles.innerHTML = '';
+  }
+
+}
+window.onload = addCircles();
+widthQuery.addListener(addCircles);
